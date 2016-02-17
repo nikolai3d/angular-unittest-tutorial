0e213bcce0fa9a1f8d2baa9fda7f318fef7e65e9
Nikolai Svakhin <svakhine@adobe.com>
2016-02-16 19:11:38 -0800

    Actual test for the resource POST interaction. Test passes, logging the POST data to console.

--- movie-app/spec/movie-core/service.spec.js
+++ movie-app/spec/movie-core/service.spec.js
@@ -1,15 +1,29 @@
-describe('MovieCore', function(){
+describe('MovieCore', function () {
     var PopularMovies;
     var $httpBackend;
 
     beforeEach(angular.mock.module('movieCore'));
 
-    beforeEach(angular.mock.inject(function(_PopularMovies_, _$httpBackend_){
+    beforeEach(angular.mock.inject(function (_PopularMovies_, _$httpBackend_) {
         PopularMovies = _PopularMovies_;
         $httpBackend = _$httpBackend_;
     }));
 
-    it('Should Create Popular Movie', function(){
+    afterEach(function () {
+
+        //This verifies that all calls came through.
+        $httpBackend.verifyNoOutstandingExpectation();
+    });
+
+    it('Should Create Popular Movie', function () {
+
+        var expectedDataValidator = function (data) {
+            dump(angular.mock.dump(data));
+            return true;
+        }
+
+        $httpBackend.expectPOST(/./, expectedDataValidator)
+            .respond(201);
 
         var popularMovie = new PopularMovies({
             movieId: 'tt0076759',

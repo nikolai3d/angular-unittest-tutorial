059e9f67575948b43a21312bbcd967bf4218c3cb
Nikolai Svakhin <svakhine@adobe.com>
2016-02-17 21:10:50 -0800

    Default HTML/Server Node app

---
+++ movie-app/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('src'));
+
+var server= app.listen((process.env.PORT || 3000), function(){
+    var port = server.address().port;
+    console.log("Example App listening on http://localhost:%s", port);
+})

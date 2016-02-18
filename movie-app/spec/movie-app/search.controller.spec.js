34b3c8a1abeaaec90443013f4f83abd4bf0dcf8c
Nikolai Svakhin <svakhine@adobe.com>
2016-02-17 19:43:51 -0800

    Refactoring: using third argument of controller() to pass query into the search function via 'this' scope

--- movie-app/spec/movie-app/search.controller.spec.js
+++ movie-app/spec/movie-app/search.controller.spec.js
@@ -1,37 +1,32 @@
 describe('', function () {
 
-    var $scope = {};
-    var $location = {};
+    //    var $scope = {};
+    var $this = {};
+    var injectedLocation = {};
+    var $controller = {};
 
     //setup
     beforeEach(angular.mock.module('mainMovieApp'));
     beforeEach(angular.mock.inject(function (_$controller_, _$location_) {
 
         //angular.mock.inject is needed so we have access to $controller service
-        $location = _$location_;
-        $scope = {};
+        injectedLocation = _$location_;
+        $controller = _$controller_;
 
-
-
-        //controllerPrototype is a controller in-line prototype here, no actual test code outside of JS exists.
-        _$controller_('SearchController', {
-            $scope: $scope,
-            $location: $location
-        });
     }));
 
     it('Should Redirect To Query Results Page For Non-Empty Query', function () {
-        $scope.query = 'star wars';
-        $scope.search();
-
-        expect($location.url()).toBe('/results?q=star%20wars');
+        //We are now using $this, and pass scope bindings in a third argument
+        $this = $controller('SearchController', {$location : injectedLocation}, { query : 'star wars'});
+        $this.search();
+        expect(injectedLocation.url()).toBe('/results?q=star%20wars');
     });
 
     it('Should NOT Redirect To Query Results Page For Empty Query', function () {
-        $scope.query = '';
-        $scope.search();
-
-        expect($location.url()).toBe('');
+        //We are now using $this, and pass scope bindings in a third argument
+        $this = $controller('SearchController', {$location : injectedLocation}, { query : ''});
+        $this.search();
+        expect(injectedLocation.url()).toBe('');
     });
 
 
--- movie-app/src/movie-app/search.controller.js
+++ movie-app/src/movie-app/search.controller.js
@@ -1,13 +1,24 @@
 
-var controllerPrototype = function (iScope, iLocation) {
+// var controllerPrototype = function (iScope, iLocation) {
+//     //iScope will refer to $scope
+//     iScope.search = function () {
+//         if (iScope.query) {
+//             //iLocation will refer to injected Angular $location service
+//             iLocation.path('/results').search('q', iScope.query);
+//         }
+//     }
+// };
+
+//Another version uses 'this' to refer to the scope
+var controllerPrototype = function (iLocation) {
     //iScope will refer to $scope
-    iScope.search = function () {
-        if (iScope.query) {
+    this.search = function () {
+        if (this.query) {
             //iLocation will refer to injected Angular $location service
-            iLocation.path('/results').search('q', iScope.query);
+            iLocation.path('/results').search('q', this.query);
         }
     }
 };
 
 angular.module('mainMovieApp', [])
-    .controller('SearchController',['$scope','$location', controllerPrototype]);
+    .controller('SearchController',['$location', controllerPrototype]);

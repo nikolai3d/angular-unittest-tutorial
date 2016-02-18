072a0725cfe207054e4f89ceedc040a1d0f6df81
Nikolai Svakhin <svakhine@adobe.com>
2016-02-17 18:41:29 -0800

    Two tests: change URL if query, no change if no query

--- movie-app/spec/movie-app/search.controller.spec.js
+++ movie-app/spec/movie-app/search.controller.spec.js
@@ -1,15 +1,36 @@
 describe('', function () {
-    it('Should Redirect To Query Results Page For Non-Empty Query', function () {
-        var $scope = {};
-        var $location = {};
 
-        $scope.search = function(){
-            $location.url = '/results?q=star%20wars';
+    var $scope = {};
+    var $location = {};
+
+    //setup
+
+    beforeEach(function () {
+        $location = {
+                url:''
         };
+        $scope = {};
 
+        $scope.search = function () {
+            if ($scope.query) {
+                $location.url = '/results?q=star%20wars';
+            }
+        };
+    });
+
+    it('Should Redirect To Query Results Page For Non-Empty Query', function () {
         $scope.query='star wars';
         $scope.search();
 
         expect($location.url).toBe('/results?q=star%20wars');
     });
+
+    it('Should NOT Redirect To Query Results Page For Empty Query', function () {
+        $scope.query='';
+        $scope.search();
+
+        expect($location.url).toBe('');
+    });
+
+
 });

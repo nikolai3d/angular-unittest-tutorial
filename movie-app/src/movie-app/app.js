6ed96ba75a9d1ff24bbcf36c9b54bdf5875333a9
Nikolai Svakhin <svakhine@adobe.com>
2016-02-18 18:42:11 -0800

    HTML connects everything

--- movie-app/src/movie-app/app.js
+++ movie-app/src/movie-app/app.js
@@ -1,1 +1,11 @@
-angular.module('mainMovieApp', ['ui.bootstrap']);
+angular.module('mainMovieApp', ['ui.bootstrap', 'ngRoute', 'omdb'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/results', {
+                templateUrl: 'movie-app/results.html',
+                controller: 'ResultsController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- movie-app/src/movie-app/search.controller.js
+++ movie-app/src/movie-app/search.controller.js
@@ -1,24 +1,24 @@
 
-// var controllerPrototype = function (iScope, iLocation) {
-//     //iScope will refer to $scope
-//     iScope.search = function () {
-//         if (iScope.query) {
-//             //iLocation will refer to injected Angular $location service
-//             iLocation.path('/results').search('q', iScope.query);
-//         }
-//     }
-// };
-
-//Another version uses 'this' to refer to the scope
-var controllerPrototype = function (iLocation) {
+var controllerPrototype = function (iScope, iLocation) {
     //iScope will refer to $scope
-    this.search = function () {
-        if (this.query) {
+    iScope.search = function () {
+        if (iScope.query) {
             //iLocation will refer to injected Angular $location service
-            iLocation.path('/results').search('q', this.query);
+            iLocation.path('/results').search('q', iScope.query);
         }
     }
 };
 
+//Another version uses 'this' to refer to the scope
+// var controllerPrototype = function (iLocation) {
+//     //iScope will refer to $scope
+//     this.search = function () {
+//         if (this.query) {
+//             //iLocation will refer to injected Angular $location service
+//             iLocation.path('/results').search('q', this.query);
+//         }
+//     }
+// };
+
 angular.module('mainMovieApp')
-    .controller('SearchController',['$location', controllerPrototype]);
+    .controller('SearchController',['$scope', '$location', controllerPrototype]);

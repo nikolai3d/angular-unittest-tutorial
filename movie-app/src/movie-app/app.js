c8426bf3dc604d4ea3513b9f7fe118d5c42a4b79
Nikolai Svakhin <svakhine@adobe.com>
2016-02-20 15:08:00 -0800

    Added ngmockE2E dependency, to set up httpBackend mock with some passthrough, to fake popular list service but passthrough on everything else. Not unit-testing, just in-browser testing

--- movie-app/src/movie-app/app.js
+++ movie-app/src/movie-app/app.js
@@ -1,4 +1,4 @@
-angular.module('mainMovieApp', ['ui.bootstrap', 'ngRoute', 'omdb', 'movieCore'])
+angular.module('mainMovieApp', ['ui.bootstrap', 'ngRoute', 'omdb', 'movieCore', 'ngMockE2E'])
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
@@ -15,4 +15,18 @@ angular.module('mainMovieApp', ['ui.bootstrap', 'ngRoute', 'omdb', 'movieCore'])
     })
     .config(function ($logProvider) {
         $logProvider.debugEnabled(false);
-    });
+    }).run(['$httpBackend', function (iFakeBackendServer){
+        var data = {fPopularResults: ['tt0076759', 'tt0080684','tt0086190']};
+        //For some reason, pure array return causes Angular Resource thingie to complain.
+        
+        var headers = {
+            headers: {'Content-Type': 'application/json'}
+        };
+
+        iFakeBackendServer.whenGET(function (s) {
+            return (s.indexOf('popular') !== -1);
+        }).respond(200, data, headers);
+
+        //Only intercept and fake the 'popular' requests, let everything else pass through.
+        iFakeBackendServer.whenGET(/.*/).passThrough();
+    }]);
--- movie-app/src/movie-app/home.controller.js
+++ movie-app/src/movie-app/home.controller.js
@@ -1,39 +1,49 @@
 angular.module('mainMovieApp')
     .controller('HomeController', ['$scope', '$interval', 'omdbApi', 'PopularMovies', '$exceptionHandler', '$log',
-    function ($scope, $interval, omdbApi, PopularMovies, iExceptionHandler, iLogging) {
-        iLogging.log('standard log');
-        iLogging.info('info log');
-        iLogging.error('error log');
-        iLogging.warn('warn log');
-        iLogging.debug('some debug information');
+        function ($scope, $interval, omdbApi, PopularMovies, iExceptionHandler, iLogging) {
+            iLogging.log('standard log');
+            iLogging.info('info log');
+            iLogging.error('error log');
+            iLogging.warn('warn log');
+            iLogging.debug('some debug information');
 
-        var fCarouselOfMovieIDs = [];
-        var fCurrentCarouselIndex = 0;
-        $scope.fCurrentMovieData = null;
-        var findAndFetchMovieDataByID = function (id) {
-            omdbApi.find(id)
-                .then(function (data) {
-                    $scope.fCurrentMovieData = data;
-                })
-                .catch(function (e) {
-                    iExceptionHandler(e);
-                })
-        };
+            var fCarouselOfMovieIDs = [];
+            var fCurrentCarouselIndex = 0;
+            $scope.fCurrentMovieData = null;
+            var findAndFetchMovieDataByID = function (id) {
+                omdbApi.find(id)
+                    .then(function (data) {
+                        $scope.fCurrentMovieData = data;
+                    })
+                    .catch(function (e) {
+                        iExceptionHandler(e);
+                    })
+            };
 
-        PopularMovies.get().then(function (data) {
-            fCarouselOfMovieIDs = data;
-            //fCarouselOfMovieIDs = ['tt0076759', 'tt0080684','tt0086190'];
-            findAndFetchMovieDataByID(fCarouselOfMovieIDs[0]);
+            // PopularMovies.get().then(function (data) {
+            //     fCarouselOfMovieIDs = data;
+            //     //fCarouselOfMovieIDs = ['tt0076759', 'tt0080684','tt0086190'];
+            //     findAndFetchMovieDataByID(fCarouselOfMovieIDs[0]);
+            //
+            //     $interval(function () {
+            //         ++fCurrentCarouselIndex;
+            //         findAndFetchMovieDataByID(fCarouselOfMovieIDs[fCurrentCarouselIndex % fCarouselOfMovieIDs.length]);
+            //     }, 5000);
+            // });
 
-            $interval(function () {
-                ++fCurrentCarouselIndex;
-                findAndFetchMovieDataByID(fCarouselOfMovieIDs[fCurrentCarouselIndex % fCarouselOfMovieIDs.length]);
-            }, 5000);
-        });
+            PopularMovies.query(function (data) {
+                fCarouselOfMovieIDs = data.fPopularResults;
+                findAndFetchMovieDataByID(fCarouselOfMovieIDs[0]);
+
+                $interval(function () {
+                    ++fCurrentCarouselIndex;
+                    findAndFetchMovieDataByID(fCarouselOfMovieIDs[fCurrentCarouselIndex % fCarouselOfMovieIDs.length]);
+                }, 5000);
+            });
 
 
+            //$scope.result = fCarouselOfMovieIDs[0];
 
-        //$scope.result = fCarouselOfMovieIDs[0];
 
-
-    }]);
+        }
+    ]);
